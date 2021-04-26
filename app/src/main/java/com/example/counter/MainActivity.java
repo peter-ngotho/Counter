a8e4842e8df3dfe9068d1ffc4257aa227795b63d
robert-muriithi <njuguiinirobert@gmail.com>
2021-04-26 22:58:18 +0300

    final commit

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int counter = 0;
+    TextView textView;
+    Button countBtn;
+    Button resetBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.number);
+        countBtn = (Button) findViewById(R.id.count);
+        resetBtn = findViewById(R.id.reset);
+
+        countBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                textView.setText(Integer.toString(counter));
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                textView.setText("0");
+            }
+        });
     }
 }
